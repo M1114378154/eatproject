f0c57810adccc2a810eef6c317a4688c4f4255cd
杨天敏 <44010251+M1114378154@users.noreply.github.com>
2019-10-22 23:20:04 +0800

    10.22注册

---
+++ src/views/Reg.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Register">
+    
+    <Register msg="好吃网注册"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Register from '@/components/Register.vue'
+
+export default {
+  name: 'reg',
+  components: {
+    Register
+  }
+}
+</script>
