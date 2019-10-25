7dc0706eb2a727f33b22ad83f7df22d2cc7e366b
杨天敏 <44010251+M1114378154@users.noreply.github.com>
2019-10-25 21:31:44 +0800

    话题区

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import Video from 'video.js'
 import 'video.js/dist/video-js.css'
-
+import { Search } from 'mint-ui';//搜索
+import { Cell } from 'mint-ui';//标题文字带链接（如：>更多）
 
 
 Vue.prototype.$video = Video
-import { Search } from 'mint-ui';//搜索
-import { Cell } from 'mint-ui';//标题文字带链接（如：>更多）
+
 
 Vue.component(Cell.name, Cell);//标题文字带链接（如：>更多）
 Vue.component(Search.name, Search);//搜索
