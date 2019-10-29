0af81587f093929aa3866d86c2164f6e8f58866f
pickJasmin <508437281@qq.com>
2019-10-29 11:52:13 +0800

    最终

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     <router-link to="/Hotfood">热门好吃推荐</router-link>
     <router-link to="/Foodstrategy">美食攻略</router-link>
     <router-link to="/Deliciousarea">好吃活动区</router-link>
-    <router-view /> -->
+    <router-view />-->
 
     <nav class="hl_nav">
       <ul class="nav_list">
@@ -26,28 +26,27 @@
             </li>
           </ul>
         </li>
-        <li class="">
-           <router-link class="approu" to="/Deliciousarea">好吃区</router-link>
+        <li class>
+          <router-link class="approu" to="/Deliciousarea">好吃区</router-link>
           <ul class="offset_left">
             <li>
-           <router-link class="approu1" to="/Gambit">好吃话题区</router-link>
+              <router-link class="approu1" to="/Gambit">好吃话题区</router-link>
             </li>
           </ul>
-           <div class="shade"></div>
+          <div class="shade"></div>
         </li>
-        <li class="">
-          <router-link class="approu" to="/Foodstrategy">美食攻略</router-link>  
+        <li class>
+          <router-link class="approu" to="/Foodstrategy">美食攻略</router-link>
         </li>
-        <li class="">
-          <router-link class="approu" to="/Mygroup">我的</router-link>  
+        <li class>
+          <router-link class="approu" to="/Mygroup">我的</router-link>
         </li>
         <li class="login">
-          <router-link class="applogin" to="/log">登录</router-link>  
+          <router-link class="applogin" to="/log" >登录</router-link>
         </li>
       </ul>
-     
     </nav>
-     <router-view />
+    <router-view />
   </div>
 </template>
 
@@ -65,188 +64,185 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  background: #eff3f5;
-}
-  .hl_nav{
-    background-color: skyblue;
-    position: relative;
-    color: #ffffff;
-    min-width: 1200px;
-    height: 50px;
-    transform: translateY(-60px);
-}
-.hl_nav .approu{
-    display: block;
-    text-decoration: none;
-    font-size: 150%;
-   margin-top: 6%;
-   color:black;
-}
-.hl_nav .applogin{
-    display: block;
-    text-decoration: none;
-    font-size: 100%;
-   margin-top: 10%;
-   /* float:right; */
-   color:black;
-}
-.hl_nav .approu:hover{
-    display: block;
-    text-decoration: none;
-    font-size: 150%;
-   margin-top: 6%;
-   color:aliceblue;
-}
-.hl_nav .applogin:hover{
-    display: block;
-    text-decoration: none;
-    font-size: 100%;
-   margin-top: 10%;
-   color:aliceblue;
-}
-.hl_nav .approu1{
-    display: block;
-    text-decoration: none;
-    font-size: 120%;
-
-   /* margin-top: 6% */
-}
-.hl_nav .shade{
-    position: absolute;
-    width: 100%;
-    height: 40px;
-    top: 50px;
-    left: 0;
-    z-index: 1;
-    background-color: #EEEEEE;
-    opacity: 0.9;
-    filter: alpha(opacity=90);
-    box-shadow: 0 5px 15px #CCCCCC;
-    display: none;
-    border-bottom: #ffffff solid 1px;
-    border-bottom: rgba(255,255,255,0.3) solid 1px;
-}
-.hl_nav .nav_list{
-    position: absolute;
-    top: 0;
-    left: 50%;
-    margin: 0 0 0 -250px;
-    z-index: 2;
-    padding: 0;
-    list-style: none;
-    width: 1200px;
-    overflow: hidden;
-    height: 50px;
-}
-.hl_nav .nav_list>li{
-    padding: 0;
-    float: left;
-    margin: 0;
-    width: 10%;
-    text-align: center;
-    height: 50px;
-}
-.hl_nav .nav_list>.login{
-    padding: 0;
-    float: right;
-    margin: 0;
-    width: 10%;
-    text-align: center;
-    height: 50px;
-}
-.hl_nav .nav_head{
-    height: 50px;
-    line-height: 50px;
-    color: #ffffff;
-    font-size: 16px;
-    position: relative;
+  /* background: #303030; */
 }
 
-.hl_nav .nav_list>li>ul{
-    position: absolute;
-    width: auto;
-    height: 40px;
-    top: 50px;
-    z-index: 2;
-    padding: 0;
-    margin: 0;
-    line-height: 40px;
-    list-style: none;
-    overflow: hidden;
-    display: none;
+.hl_nav {
+  background-color: skyblue;
+  position: relative;
+  color: #ffffff;
+  /* width: 100%; */
+  height: 50px;
+  transform: translateY(-60px);
+  list-style: none;
 }
-.hl_nav .nav_list>li.active {
-    background-color: #0099cc;
-  
+.hl_nav .approu {
+  display: block;
+  text-decoration: none;
+  font-size: 150%;
+  margin-top: 6%;
+  color: black;
 }
-.hl_nav .nav_list>li:hover{
+.hl_nav .applogin {
+  display: block;
+  text-decoration: none;
+  font-size: 100%;
+  margin-top: 10%;
+  /* float:right; */
+  color: black;
+}
+.hl_nav .approu:hover {
+  display: block;
+  text-decoration: none;
+  font-size: 150%;
+  margin-top: 6%;
+  color: aliceblue;
+}
+.hl_nav .applogin:hover {
+  display: block;
+  text-decoration: none;
+  font-size: 100%;
+  margin-top: 10%;
+  color: aliceblue;
+}
+.hl_nav .approu1 {
+  display: block;
+  text-decoration: none;
+  font-size: 120%;
 
-    background-color: #0099cc;
-
+  /* margin-top: 6% */
 }
-.hl_nav .nav_list>li:hover>ul{
-    display: block;
-    color: white;
-
+.hl_nav .shade {
+  position: absolute;
+  width: 100%;
+  height: 40px;
+  top: 50px;
+  left: 0;
+  z-index: 1;
+  background-color: #eeeeee;
+  opacity: 0.9;
+  filter: alpha(opacity=90);
+  box-shadow: 0 5px 15px #cccccc;
+  display: none;
+  border-bottom: #ffffff solid 1px;
+  border-bottom: rgba(255, 255, 255, 0.3) solid 1px;
 }
-
-.hl_nav .nav_list>li>ul.offset_left{
-    left: 0;
+.hl_nav .nav_list {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  margin: 0 0 0 -250px;
+  z-index: 2;
+  padding: 0;
+  list-style: none;
+  width: 1200px;
+  overflow: hidden;
+  height: 50px;
 }
-.hl_nav .nav_list>li>ul.offset_left.offset_1{
-    left: 10%;
+.hl_nav .nav_list > li {
+  padding: 0;
+  float: left;
+  margin: 0;
+  width: 10%;
+  text-align: center;
+  height: 50px;
 }
-.hl_nav .nav_list>li>ul.offset_left.offset_2{
-    left: 20%;
+.hl_nav .nav_list > .login {
+  padding: 0;
+  float: right;
+  margin: 0;
+  width: 10%;
+  text-align: center;
+  height: 50px;
 }
-.hl_nav .nav_list>li>ul.offset_left.offset_3{
-    left: 30%;
-}
-.hl_nav .nav_list>li>ul.offset_left.offset_4{
-    left: 40%;
-}
-.hl_nav .nav_list>li>ul.offset_left.offset_5{
-    left: 50%;
-}
-.hl_nav .nav_list>li>ul.offset_left.offset_6{
-    left: 60%;
-}
-.hl_nav .nav_list>li>ul.offset_left.offset_7{
-    left: 70%;
-}
-.hl_nav .nav_list>li>ul.offset_left.offset_8{
-    left: 80%;
-}
-.hl_nav .nav_list>li>ul.offset_right{
-    right: 0;
-}
-.hl_nav .nav_list>li>ul.offset_right.offset_1{
-    right: 10%;
-}
-.hl_nav .nav_list>li>ul.offset_right.offset_2{
-    right: 20%;
-}
-
-
-.hl_nav .nav_list>li>ul>li{
-    float: left;
-}
-.hl_nav .nav_list>li>ul>li>a{
-    text-decoration: none;
-    display: block;
-    color: #666666;
-    padding: 0 20px;
-    height: 40px;
-}
-.hl_nav .nav_list>li>ul>li>a:hover{
-    background-color: #ffffff;
-    color: #999999;
-}
-.hl_nav .nav_list:hover{
-    height: 90px;
+.hl_nav .nav_head {
+  height: 50px;
+  line-height: 50px;
+  color: #ffffff;
+  font-size: 16px;
+  position: relative;
 }
 
-.hl_nav .nav_list:hover + .shade{
-    display: block;
+.hl_nav .nav_list > li > ul {
+  position: absolute;
+  width: auto;
+  height: 40px;
+  top: 50px;
+  z-index: 2;
+  padding: 0;
+  margin: 0;
+  line-height: 40px;
+  list-style: none;
+  overflow: hidden;
+  display: none;
+}
+.hl_nav .nav_list > li.active {
+  background-color: #0099cc;
+}
+.hl_nav .nav_list > li:hover {
+  background-color: #0099cc;
+}
+.hl_nav .nav_list > li:hover > ul {
+  display: block;
+  color: white;
+}
+
+.hl_nav .nav_list > li > ul.offset_left {
+  left: 0;
+}
+.hl_nav .nav_list > li > ul.offset_left.offset_1 {
+  left: 10%;
+}
+.hl_nav .nav_list > li > ul.offset_left.offset_2 {
+  left: 20%;
+}
+.hl_nav .nav_list > li > ul.offset_left.offset_3 {
+  left: 30%;
+}
+.hl_nav .nav_list > li > ul.offset_left.offset_4 {
+  left: 40%;
+}
+.hl_nav .nav_list > li > ul.offset_left.offset_5 {
+  left: 50%;
+}
+.hl_nav .nav_list > li > ul.offset_left.offset_6 {
+  left: 60%;
+}
+.hl_nav .nav_list > li > ul.offset_left.offset_7 {
+  left: 70%;
+}
+.hl_nav .nav_list > li > ul.offset_left.offset_8 {
+  left: 80%;
+}
+.hl_nav .nav_list > li > ul.offset_right {
+  right: 0;
+}
+.hl_nav .nav_list > li > ul.offset_right.offset_1 {
+  right: 10%;
+}
+.hl_nav .nav_list > li > ul.offset_right.offset_2 {
+  right: 20%;
+}
+
+.hl_nav .nav_list > li > ul > li {
+  float: left;
+}
+.hl_nav .nav_list > li > ul > li > a {
+  text-decoration: none;
+  display: block;
+  color: #666666;
+  padding: 0 20px;
+  height: 40px;
+}
+.hl_nav .nav_list > li > ul > li > a:hover {
+  background-color: #ffffff;
+  color: #999999;
+}
+.hl_nav .nav_list:hover {
+  height: 90px;
+}
+
+.hl_nav .nav_list:hover + .shade {
+  display: block;
 }
 </style>
--- src/components/Foodstrategy.vue
+++ src/components/Foodstrategy.vue
@@ -1,32 +1,21 @@
 <template>
   <div class="Foodstrategy">
     <div class="container1">
-      <!-- <ul class="gyul">
-        <li class="gyli">
-          <a class="active" href="#home">寻找每日新菜馆</a>
-        </li>
-        <li class="gyli">
-          <a href="#news">网红人气菜</a>
-        </li>
-        <li class="gyli">
-          <a href="#contact">到店觅食</a>
-        </li>
-      </ul> -->
-      <div class="Foodtop">
-      <center>
-        <ul class="Fcateul">
-          <li class="Fcateli">
-            <a class="active" href="#">寻找每日新菜馆</a>
-          </li>
-          <li class="Fcateli">
-            <a  href="#">网红人气菜</a>
-          </li>
-          <li class="Fcateli">
-            <a href="#">到店觅食</a>
-          </li>
-        </ul>
-      </center>
-    </div>
+      <!-- <div class="Foodtop">
+        <center>
+          <ul class="Fcateul">
+            <li class="Fcateli">
+              <a class="active" href="#">寻找每日新菜馆</a>
+            </li>
+            <li class="Fcateli">
+              <a href="#">网红人气菜</a>
+            </li>
+            <li class="Fcateli">
+              <a href="#">到店觅食</a>
+            </li>
+          </ul>
+        </center>
+      </div> -->
       <div class="gamsearch-2">
         <input class="gaminput" type="text" placeholder="搜索新菜馆" name id value />
         <button>
@@ -101,29 +90,29 @@
                 style="width: 200px; text-decoration: none; color: skyblue;"
               >查看详情</a>
             </p>
-          </div> -->
-      <div class="row">
-      <p  align="left" style="color:gray;">特色</p>
-      </div>
-   <p class="food" align="right">
-      &nbsp;&nbsp;&nbsp;
-      <svg
-        t="1572254204369"
-        class="icon"
-        viewBox="0 0 1024 1024"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        p-id="2028"
-        width="20"
-        height="20"
-      >
-        <path
-          d="M214.60224 997.832a30.744 30.744 0 0 1-18.104-5.88 30.76 30.76 0 0 1-12.192-30.088l54.04-313.832L9.36224 425.768a30.736 30.736 0 0 1 17-52.48l316.576-45.84L484.49024 41.864c10.328-20.928 44.736-20.928 55.064 0L681.14624 327.448l316.56 45.84a30.736 30.736 0 0 1 17 52.48L785.71424 648.032l54.072 313.832a30.8 30.8 0 0 1-12.184 30.088 30.904 30.904 0 0 1-32.368 2.376L512.02624 846.096 228.85824 994.32a30.864 30.864 0 0 1-14.256 3.512zM512.02624 780.64c4.896 0 9.792 1.168 14.264 3.512l242.344 126.84-46.272-268.512a30.688 30.688 0 0 1 8.888-27.288l195.784-190.016-270.688-39.216a30.672 30.672 0 0 1-23.12-16.752L512.02624 124.744 390.85024 369.208a30.672 30.672 0 0 1-23.12 16.752l-270.704 39.208 195.784 190.016a30.728 30.728 0 0 1 8.888 27.288l-46.232 268.52L497.76224 784.16A30.712 30.712 0 0 1 512.02624 780.64z"
-          fill="#838384"
-          p-id="2029"
-        />
-      </svg>140
-       </p>
+          </div>-->
+          <div class="row">
+            <p align="left" style="color:gray;">特色</p>
+          </div>
+          <p class="food" align="right">
+            &nbsp;&nbsp;&nbsp;
+            <svg
+              t="1572254204369"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="2028"
+              width="20"
+              height="20"
+            >
+              <path
+                d="M214.60224 997.832a30.744 30.744 0 0 1-18.104-5.88 30.76 30.76 0 0 1-12.192-30.088l54.04-313.832L9.36224 425.768a30.736 30.736 0 0 1 17-52.48l316.576-45.84L484.49024 41.864c10.328-20.928 44.736-20.928 55.064 0L681.14624 327.448l316.56 45.84a30.736 30.736 0 0 1 17 52.48L785.71424 648.032l54.072 313.832a30.8 30.8 0 0 1-12.184 30.088 30.904 30.904 0 0 1-32.368 2.376L512.02624 846.096 228.85824 994.32a30.864 30.864 0 0 1-14.256 3.512zM512.02624 780.64c4.896 0 9.792 1.168 14.264 3.512l242.344 126.84-46.272-268.512a30.688 30.688 0 0 1 8.888-27.288l195.784-190.016-270.688-39.216a30.672 30.672 0 0 1-23.12-16.752L512.02624 124.744 390.85024 369.208a30.672 30.672 0 0 1-23.12 16.752l-270.704 39.208 195.784 190.016a30.728 30.728 0 0 1 8.888 27.288l-46.232 268.52L497.76224 784.16A30.712 30.712 0 0 1 512.02624 780.64z"
+                fill="#838384"
+                p-id="2029"
+              />
+            </svg>140
+          </p>
         </div>
       </div>
 
@@ -149,29 +138,29 @@
                 style="width: 200px; text-decoration: none; color: skyblue;"
               >查看详情</a>
             </p>
-          </div> -->
-     <div class="row">
-      <p  align="left" style="color:gray;">菜单</p>
-      </div>
-   <p class="food" align="right" >
-      &nbsp;&nbsp;&nbsp;
-      <svg
-        t="1572254204369"
-        class="icon"
-        viewBox="0 0 1024 1024"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        p-id="2028"
-        width="20"
-        height="20"
-      >
-        <path
-          d="M214.60224 997.832a30.744 30.744 0 0 1-18.104-5.88 30.76 30.76 0 0 1-12.192-30.088l54.04-313.832L9.36224 425.768a30.736 30.736 0 0 1 17-52.48l316.576-45.84L484.49024 41.864c10.328-20.928 44.736-20.928 55.064 0L681.14624 327.448l316.56 45.84a30.736 30.736 0 0 1 17 52.48L785.71424 648.032l54.072 313.832a30.8 30.8 0 0 1-12.184 30.088 30.904 30.904 0 0 1-32.368 2.376L512.02624 846.096 228.85824 994.32a30.864 30.864 0 0 1-14.256 3.512zM512.02624 780.64c4.896 0 9.792 1.168 14.264 3.512l242.344 126.84-46.272-268.512a30.688 30.688 0 0 1 8.888-27.288l195.784-190.016-270.688-39.216a30.672 30.672 0 0 1-23.12-16.752L512.02624 124.744 390.85024 369.208a30.672 30.672 0 0 1-23.12 16.752l-270.704 39.208 195.784 190.016a30.728 30.728 0 0 1 8.888 27.288l-46.232 268.52L497.76224 784.16A30.712 30.712 0 0 1 512.02624 780.64z"
-          fill="#838384"
-          p-id="2029"
-        />
-      </svg>140
-       </p>
+          </div>-->
+          <div class="row">
+            <p align="left" style="color:gray;">菜单</p>
+          </div>
+          <p class="food" align="right">
+            &nbsp;&nbsp;&nbsp;
+            <svg
+              t="1572254204369"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="2028"
+              width="20"
+              height="20"
+            >
+              <path
+                d="M214.60224 997.832a30.744 30.744 0 0 1-18.104-5.88 30.76 30.76 0 0 1-12.192-30.088l54.04-313.832L9.36224 425.768a30.736 30.736 0 0 1 17-52.48l316.576-45.84L484.49024 41.864c10.328-20.928 44.736-20.928 55.064 0L681.14624 327.448l316.56 45.84a30.736 30.736 0 0 1 17 52.48L785.71424 648.032l54.072 313.832a30.8 30.8 0 0 1-12.184 30.088 30.904 30.904 0 0 1-32.368 2.376L512.02624 846.096 228.85824 994.32a30.864 30.864 0 0 1-14.256 3.512zM512.02624 780.64c4.896 0 9.792 1.168 14.264 3.512l242.344 126.84-46.272-268.512a30.688 30.688 0 0 1 8.888-27.288l195.784-190.016-270.688-39.216a30.672 30.672 0 0 1-23.12-16.752L512.02624 124.744 390.85024 369.208a30.672 30.672 0 0 1-23.12 16.752l-270.704 39.208 195.784 190.016a30.728 30.728 0 0 1 8.888 27.288l-46.232 268.52L497.76224 784.16A30.712 30.712 0 0 1 512.02624 780.64z"
+                fill="#838384"
+                p-id="2029"
+              />
+            </svg>140
+          </p>
         </div>
       </div>
     </div>
@@ -194,8 +183,8 @@ export default {
   font-size: 20px;
   color: #ccc;
   cursor: pointer;
-   margin-top:-37px; 
-   border-bottom: solid 1px gray;
+  margin-top: -37px;
+  border-bottom: solid 1px gray;
   /* margin-left: 80%; */
 }
 /* .gyul {
@@ -229,7 +218,6 @@ export default {
   background-color: skyblue;
 } */
 .Foodtop {
-
   margin-top: 1px; /*离顶部的距离为0*/
   margin-bottom: 5px;
   margin: 0%;
@@ -260,8 +248,8 @@ export default {
   font-size: 190%;
   /* height: 48px; */
 }
-.Foodtop .Fcateli a.active{
-    background-color: skyblue;
+.Foodtop .Fcateli a.active {
+  background-color: skyblue;
 }
 .Foodtop .Fcateli a:hover {
   /* 鼠标选中时背景变为黑色 */
@@ -277,7 +265,7 @@ export default {
   width: 100%;
   height: 270px;
   background-color: whitesmoke;
-   border-bottom: solid 1px gray;
+  border-bottom: solid 1px gray;
 }
 
 .flex-item-3 {
@@ -288,8 +276,8 @@ export default {
 }
 
 .gamsearch-2 {
-  width: 80%;      
-  margin:1px auto;
+  width: 80%;
+  margin: 1px auto;
   display: flex;
   /*border: 1px solid red;*/
   transform: translateY(-6px);
